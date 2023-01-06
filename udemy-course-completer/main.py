4b54adc818db1ee99fef5ccd897af336ec49e110
eshabaweja <bawejaesha02@gmail.com>
2023-01-06 12:07:47 +0530

    just for fun

---
+++ udemy-course-completer/main.py
@@ -0,0 +1,9 @@
+import os
+from selenium import webdriver
+
+UDEMY_COURSE_URL="from .env"
+UDEMY_EMAIL = "from .env"
+UDEMY_PASSWORD = "from .env"
+
+UDEMY_ACCORDION_CLASS = "accordion-panel--panel--24beS"
+UDEMY_CHECKBOX_CLASS = "ud-fake-toggle-checkbox"
