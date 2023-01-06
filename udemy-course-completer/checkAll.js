0a069db2f9ef2e15f8084a24fa5f5ee693706d28
eshabaweja <bawejaesha02@gmail.com>
2023-01-06 17:37:43 +0530

    ✨ open accordions and check all boxes

--- udemy-course-completer/checkAll.js
+++ udemy-course-completer/checkAll.js
@@ -1,6 +1,14 @@
-// paste this in console AFTER opening all accordions
-
-checkboxes=document.querySelectorAll(".ud-real-toggle-input")
+// open all accordions
+// assuming all accordions are initially closed
+accordions = document.querySelectorAll(".accordion-panel--panel-toggler--UP46Q")
+for (var i = 0, len = accordions.length; i < len; i=i+2) {
+    accordions[i].click();
+}
+// check (click) all unchecked boxes
+checkboxes = document.querySelectorAll(".ud-real-toggle-input")
 for (var i = 0, len = checkboxes.length; i < len; i++) {
-    checkboxes[i].checked=true
-} 
+    checkbox = checkboxes[i]
+    if (checkbox.checked == false) {
+        checkbox.click();
+    }
+}
