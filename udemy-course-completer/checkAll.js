cbc1ea1517deefe428298ab9990b75a57890f7c9
eshabaweja <bawejaesha02@gmail.com>
2023-01-06 17:13:48 +0530

    feat: complete udemy course

---
+++ udemy-course-completer/checkAll.js
@@ -0,0 +1,6 @@
+// paste this in console AFTER opening all accordions
+
+checkboxes=document.querySelectorAll(".ud-real-toggle-input")
+for (var i = 0, len = checkboxes.length; i < len; i++) {
+    checkboxes[i].checked=true
+} 
